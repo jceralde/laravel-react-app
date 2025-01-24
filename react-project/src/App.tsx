c4547ef63e6e0ea167a01121385ce119a6c54f7c
jceralde <rem.ceralde@gmail.com>
2025-01-24 13:37:56 +0800

    Add navlink and main section

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,18 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Layout from './layout/Layout'
 import Home from './pages/Home'
-import About from './pages/About'
+import HCharge from './pages/HCharge'
+
+
+
+import { ThemeProvider } from '@/components/theme-provider'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about/:id?" element={<About />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="light">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="hcharge" element={<HCharge />} />
+          </Route>
+          {/* 
+          <Route path="/other" element={<Layout2 />}>
+            <Route index element={<Home />} />
+          </Route> */}
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
--- react-project/src/layout/Layout.tsx
+++ react-project/src/layout/Layout.tsx
@@ -1,24 +1,31 @@
-import { Link, Outlet } from 'react-router'
+import { Link, NavLink, Outlet } from 'react-router'
 
-import { Button } from '@/components/ui/button'
+import Header from '@/components/header/Header'
+import Logo from '@/components/header/Logo'
+import { ModeToggle } from '@/components/mode-toggle'
 
 const Layout = () => {
   return (
-    <div>
-      <header style={{ padding: '1rem', backgroundColor: '#f0f0f0' }}>
-        <nav>
-          <Link to="/" style={{ marginRight: '1rem' }}>
-            Home
-          </Link>
-          <Link to="/about">
-            <Button>About</Button>
-          </Link>
+    <div className='relative flex flex-1 flex-col'>
+      <Header>
+        <Link to="/">
+          <Logo />
+        </Link>
+        <ModeToggle />
+      </Header>
+
+      <div className='px-4 flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10'>
+        <nav className='p-4 overflow-auto flex flex-col gap-4 shrink-0 min-h-[calc(100svh-56px)] border-r border-gray-100'>
+          <NavLink
+            to='/hcharge'
+          >
+            HCharge
+          </NavLink>
         </nav>
-      </header>
-      <main style={{ padding: '1rem' }}>
-        <Outlet /> {/* Render the child route here */}
-      </main>
-      <footer style={{ padding: '1rem', backgroundColor: '#f0f0f0', marginTop: '2rem' }}>Footer Content</footer>
+        <main className='py-4'>
+          <Outlet />
+        </main>
+      </div>
     </div>
   )
 }
